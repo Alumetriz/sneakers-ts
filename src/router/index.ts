7511481f8136aac95e8a96f19b5d90769e854693
Alumetriz <fanmax0808@gmail.com>
2023-12-27 19:06:08 +0200

    feat: project configuration

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    // TODO add routes
+    // {
+    //   path: '/',
+    //   component: Home
+    // }
+  ]
+})
