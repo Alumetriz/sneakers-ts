1e4a2e81f42f60a23599eb5449fe029ec3825e01
Alumetriz <fanmax0808@gmail.com>
2023-12-27 20:40:44 +0200

    refactor: change header images path

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -4,7 +4,7 @@
   <header class="flex justify-between items-center border-b border-[#EAEAEA] p-11">
     <router-link to="/">
       <div class="flex gap-3.5 items-center">
-        <img src="../../public/images/logo.png" alt="Logo" class="h-10 w-10" />
+        <img src="/images/logo.png" alt="Logo" class="h-10 w-10" />
         <div>
           <h1 class="font-bold text-xl uppercase">Sneakers</h1>
           <p class="text-[#9D9D9D]">Магазин лучших кроссовок</p>
@@ -17,7 +17,7 @@
         <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
           <router-link to="/orders">
             <div class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-              <img src="../../public/images/cart.svg" alt="Корзина" />
+              <img src="/images/cart.svg" alt="Корзина" />
               <span class="font-bold text-black">Корзина</span>
             </div>
           </router-link>
@@ -25,14 +25,14 @@
         <li>
           <router-link to="/favorites">
             <div class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
-              <img src="../../public/images/heart.svg" alt="Закладки" />
+              <img src="/images/heart.svg" alt="Закладки" />
               <span class="text-[#5C5C5C] font-normal">Закладки</span>
             </div>
           </router-link>
         </li>
         <li class="flex items-center cursor-pointer gap-3 text-gray-500 hover:text-black">
           <a href="#" class="flex gap-3">
-            <img src="../../public/images/profile.svg" alt="Профиль" />
+            <img src="/images/profile.svg" alt="Профиль" />
             <span class="text-[#5C5C5C] font-normal">Профиль</span>
           </a>
         </li>
