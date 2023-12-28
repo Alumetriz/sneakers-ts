10ac2652acbe560f560cef319b8921dbe8e02e87
Alumetriz <fanmax0808@gmail.com>
2023-12-28 17:00:12 +0200

    feat: add search and sorting of products

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -17,17 +17,33 @@ const props = defineProps({
 
 const productsStore = useProductsStore()
 
-onMounted(() => {
-  productsStore.fetchData()
+const filteredProducts = computed((): Product[] => {
+  const searchParams = props.searchParam.toLowerCase().split(' ')
+
+  return productsStore.list
+    .filter((product) => {
+      const title = product.title.toLowerCase()
+      return searchParams.every((param) => title.includes(param))
+    })
+    .sort((a, b) => {
+      if (props.sortParam === 'name_asc') {
+        return a.title.localeCompare(b.title)
+      } else if (props.sortParam === 'price_asc') {
+        return a.price - b.price
+      } else if (props.sortParam === 'price_desc') {
+        return b.price - a.price
+      }
+      return 0
+    })
 })
 
-const filteredProducts = computed((): Product[] => {
-  return productsStore.list.filter((product) => product.title.includes(props.searchParam))
+onMounted(() => {
+  productsStore.fetchData()
 })
 </script>
 
 <template>
-  <div class="grid-cols-4 grid gap-10 mt-8">
+  <div class="grid-cols-4 grid gap-10 mt-8" v-auto-animate>
     <card-item v-for="product in filteredProducts" :key="product.id" :product="product"></card-item>
   </div>
 </template>
