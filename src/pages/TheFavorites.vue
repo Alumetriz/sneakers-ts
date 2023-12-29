b1a7f5b28e96eb645fdea6c14db65a03fec09256
Alumetriz <fanmax0808@gmail.com>
2023-12-29 13:09:30 +0200

    feat: add content to favorites page

--- src/pages/TheFavorites.vue
+++ src/pages/TheFavorites.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { ChevronLeftIcon } from '@heroicons/vue/24/solid'
+import { ChevronLeftIcon, ArrowLeftIcon } from '@heroicons/vue/24/solid'
 import FavoritesCardsList from '@/components/FavoritesCardsList.vue'
+import { useFavoritesStore } from '@/stores/FavoritesStore'
+
+const favoritesStore = useFavoritesStore()
 </script>
 
 <template>
@@ -14,22 +17,22 @@ import FavoritesCardsList from '@/components/FavoritesCardsList.vue'
       <h1 class="font-bold text-3xl">Мои закладки</h1>
     </div>
 
-    <favorites-cards-list></favorites-cards-list>
-    <!--    <div v-else class="flex flex-col gap-3 items-center justify-center h-full p-11">-->
-    <!--      <img src="@/assets/img/emoji-1.png" alt="" class="h-16 w-16" />-->
-    <!--      <h3 class="font-semibold text-2xl">Закладок нет :(</h3>-->
-    <!--      <p class="text-lg text-[#9D9D9D] max-w-sm text-center">Вы ничего не добавляли в закладки</p>-->
-    <!--      <router-link to="/">-->
-    <!--        <button-->
-    <!--          class="relative bg-lime-500 hover:bg-lime-600 active:bg-lime-700 disabled:bg-slate-300 text-white w-[250px] rounded-2xl p-5 mt-3"-->
-    <!--        >-->
-    <!--          <span class="flex items-center justify-center gap-5 font-semibold text-xl">-->
-    <!--            <ArrowLeftIcon class="w-6 h-6 transition arrow-icon-left" />-->
-    <!--            Вернуться назад-->
-    <!--          </span>-->
-    <!--        </button>-->
-    <!--      </router-link>-->
-    <!--    </div>-->
+    <favorites-cards-list v-if="favoritesStore.list.length"></favorites-cards-list>
+    <div v-else class="flex flex-col gap-3 items-center justify-center h-full p-11">
+      <img src="/images/emoji-1.png" alt="" class="h-16 w-16" />
+      <h3 class="font-semibold text-2xl">Закладок нет :(</h3>
+      <p class="text-lg text-[#9D9D9D] max-w-sm text-center">Вы ничего не добавляли в закладки</p>
+      <router-link to="/">
+        <button
+          class="relative bg-lime-500 hover:bg-lime-600 active:bg-lime-700 disabled:bg-slate-300 text-white w-[250px] rounded-2xl p-5 mt-3"
+        >
+          <span class="flex items-center justify-center gap-5 font-semibold text-xl">
+            <ArrowLeftIcon class="w-6 h-6 transition arrow-icon-left" />
+            Вернуться назад
+          </span>
+        </button>
+      </router-link>
+    </div>
   </main>
 </template>
 
